fede5ef752fbea62a7c860d67e37737c74da8383
Petar Dochev <petar.dochev@sap.com>
2018-05-22 15:04:51 +0300

    Add uuid to plugin api

--- vendor/github.com/dotchev/go-main/plugin/plugin.go
+++ vendor/github.com/dotchev/go-main/plugin/plugin.go
@@ -1,6 +1,18 @@
 package plugin
 
+import (
+	uuid "github.com/satori/go.uuid"
+)
+
 type PluginRequest struct {
 	Name string
 	Size int
+}
+
+func UUID() string {
+	id, err := uuid.NewV4()
+	if err != nil {
+		panic(err)
+	}
+	return id.String()
 }
